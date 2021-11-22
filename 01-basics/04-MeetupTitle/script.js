a823f0e1195bdb5708562fb278ed11a27d72d7a5
7xLisyonok <7x.Lisyonok@gmail.com>
2021-11-22 20:46:32 +1000

    solve 04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
 function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
+    return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return response.json().then((error) => {
+                throw error;
+            });
+        }
+    });
 }
 
-// Требуется создать Vue приложение
+const app = createApp({
+    data() {
+        return {
+            meetupId: 1,
+            meetup: '',
+        };
+    },
+    watch: {
+        meetupId: {
+            immediate: true,
+            handler(newVal, oldVal) {
+                fetchMeetupById(newVal).then((e) => (this.meetup = e));
+            },
+        },
+    },
+});
+
+const vm = app.mount('#app');
