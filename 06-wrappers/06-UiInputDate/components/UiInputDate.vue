d8213d2316fb8e2c00c8d9bc2e0766e2316e08a9
7xLisyonok <7x.Lisyonok@gmail.com>
2021-12-10 11:42:15 +1000

    solve 06-UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,13 +1,71 @@
 <template>
-  <ui-input />
+  <ui-input ref="uiInput" v-model="modelValueFormatted" :type="type">
+    <template v-if="$slots['left-icon']" #left-icon>
+      <slot name="left-icon" />
+    </template>
+    <template v-if="$slots['right-icon']" #right-icon>
+      <slot name="left-icon" />
+    </template>
+  </ui-input>
 </template>
 
 <script>
+import moment from 'moment';
 import UiInput from './UiInput';
+
+const DateTypeList = ['date', 'time', 'datetime-local'];
 
 export default {
   name: 'UiInputDate',
 
   components: { UiInput },
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator: (value) => DateTypeList.includes(value),
+    },
+
+    modelValue: {
+      default: null,
+    },
+  },
+
+  emits: ['change', 'update:modelValue'],
+
+  data() {
+    return {
+      valueAsNumber: this.modelValue,
+    };
+  },
+
+  computed: {
+    dateTypeFormat() {
+      if (this.type === 'time') {
+        const step = this.$attrs.step;
+        if (step && +step % 60 !== 0) {
+          return 'HH:mm:ss';
+        }
+        return 'HH:mm';
+      }
+
+      if (this.type === 'datetime-local') {
+        return 'YYYY-MM-DDTHH:mm';
+      }
+
+      return 'YYYY-MM-DD';
+    },
+
+    modelValueFormatted: {
+      get() {
+        return moment(this.modelValue).utc().format(this.dateTypeFormat);
+      },
+      set(newValue) {
+        const newModelValue = this.$refs.uiInput.$refs.input.valueAsNumber;
+        this.$emit('update:modelValue', newModelValue);
+      },
+    },
+  },
 };
 </script>
