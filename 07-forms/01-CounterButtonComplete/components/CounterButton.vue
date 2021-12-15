72258ff9ae5294d06c74beb8a4c16db9f107d21e
7xLisyonok <7x.Lisyonok@gmail.com>
2021-12-15 11:50:49 +1000

    solve 01-CounterButtonComplete

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,36 @@
 <template>
-  <button></button>
+  <button @click="incCount">{{ innerCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      innerCount: this.count,
+    };
+  },
+
+  watch: {
+    count(newValue) {
+      this.innerCount = newValue;
+    },
+  },
+
+  methods: {
+    incCount() {
+      this.$emit('update:count', ++this.innerCount);
+    },
+  },
 };
 </script>
